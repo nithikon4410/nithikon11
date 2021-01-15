be0a371abc23eeeb6e9a24465a73aeac64d4535a
nithikon4410 <nithikon_jansanitsri@cmu.ac.th>
2021-01-15 15:38:25 +0700

    lab11_2.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+long long int fibonacci(int );
 
 int main(){
     int x;
@@ -7,3 +8,10 @@ int main(){
     cout << fibonacci(x);
     return 0; 
 }
+long long int fibonacci(int x){
+    if(x<=1){
+        return x;
+    }else{
+        return fibonacci(x-1)+fibonacci(x-2);
+    }
+}    
